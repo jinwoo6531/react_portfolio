c124e452b6e1fdb6e931ea2d31d991e5d5900d5d
jinwoo <jintakk6531@gmail.com>
2020-11-17 15:20:22 +0900

    Updates

---
+++ precache-manifest.2fb19189c1b25b0272553d3ff326a02b.js
@@ -0,0 +1,130 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6df31ce362158adafad139165a9b0488",
+    "url": "/react_portfolio/index.html"
+  },
+  {
+    "revision": "49fe02e7f61624a14981",
+    "url": "/react_portfolio/static/css/main.e9979398.chunk.css"
+  },
+  {
+    "revision": "d6fb2a6a3956387c7c8e",
+    "url": "/react_portfolio/static/js/2.115e428d.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/react_portfolio/static/js/2.115e428d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "49fe02e7f61624a14981",
+    "url": "/react_portfolio/static/js/main.18a98148.chunk.js"
+  },
+  {
+    "revision": "76238f47f579940f7b2f",
+    "url": "/react_portfolio/static/js/runtime-main.eb05d73a.js"
+  },
+  {
+    "revision": "2757facf98642f899a81bc5a4425fcb3",
+    "url": "/react_portfolio/static/media/KanbanBoard.2757facf.png"
+  },
+  {
+    "revision": "f5711eaa4996e1483f221e7c57cb7905",
+    "url": "/react_portfolio/static/media/algo.f5711eaa.svg"
+  },
+  {
+    "revision": "63f68ccc85cc2ab7da913353c08cccaf",
+    "url": "/react_portfolio/static/media/api.63f68ccc.svg"
+  },
+  {
+    "revision": "6b5433ec6558cf134d5f6c33b724bac2",
+    "url": "/react_portfolio/static/media/chatapp.6b5433ec.JPG"
+  },
+  {
+    "revision": "12314f2d8dfb35762e525cdc8e0728bc",
+    "url": "/react_portfolio/static/media/color_classification.12314f2d.JPG"
+  },
+  {
+    "revision": "ca042db5d0a17f1f6ee36ea90fa2f3a4",
+    "url": "/react_portfolio/static/media/computer.ca042db5.svg"
+  },
+  {
+    "revision": "0a27bbff6aa4dfb3bf8606c2fba69fd6",
+    "url": "/react_portfolio/static/media/facebook.0a27bbff.svg"
+  },
+  {
+    "revision": "98f8fb2c700f7bca926de1dc1ea21cd1",
+    "url": "/react_portfolio/static/media/github.98f8fb2c.svg"
+  },
+  {
+    "revision": "539fd92d57d90dcbbb9aa3820998d9b3",
+    "url": "/react_portfolio/static/media/hardware_store.539fd92d.JPG"
+  },
+  {
+    "revision": "d08bce0dcb53ad1ef20b0cae18977145",
+    "url": "/react_portfolio/static/media/instagram.d08bce0d.svg"
+  },
+  {
+    "revision": "c5e13bb2d05c7db7c36245a2b7d5012c",
+    "url": "/react_portfolio/static/media/js.c5e13bb2.svg"
+  },
+  {
+    "revision": "83dc5b5743f0815544de5a22fdedee07",
+    "url": "/react_portfolio/static/media/mapProject.83dc5b57.png"
+  },
+  {
+    "revision": "4ca0da55f9f84543d825c66b0940d85d",
+    "url": "/react_portfolio/static/media/movieMain.4ca0da55.png"
+  },
+  {
+    "revision": "f97f647d2d9707febf824cfa79cc7ff8",
+    "url": "/react_portfolio/static/media/movie_search.f97f647d.png"
+  },
+  {
+    "revision": "08182d19de06998c084f7968277e2240",
+    "url": "/react_portfolio/static/media/new_year.08182d19.JPG"
+  },
+  {
+    "revision": "ef9d13e12b064ef5213696f0814a1bcd",
+    "url": "/react_portfolio/static/media/pin.ef9d13e1.svg"
+  },
+  {
+    "revision": "31d141e3d133007620a912f6cfa12fda",
+    "url": "/react_portfolio/static/media/profile.31d141e3.png"
+  },
+  {
+    "revision": "b5fac2be7a6a8edc808f550a24217391",
+    "url": "/react_portfolio/static/media/puzzle.b5fac2be.svg"
+  },
+  {
+    "revision": "26c488dd1be55304e547e30598fb5ca8",
+    "url": "/react_portfolio/static/media/python.26c488dd.svg"
+  },
+  {
+    "revision": "2e01c74fe6fcd933e0e9f338e56c0922",
+    "url": "/react_portfolio/static/media/react.2e01c74f.svg"
+  },
+  {
+    "revision": "1464053c658e62318c689e56c4f1643c",
+    "url": "/react_portfolio/static/media/repair.1464053c.svg"
+  },
+  {
+    "revision": "6368fd7fe1688c46079546a1bf7637f4",
+    "url": "/react_portfolio/static/media/resume.6368fd7f.pdf"
+  },
+  {
+    "revision": "45b48090de6a5fe37055f98339319dc4",
+    "url": "/react_portfolio/static/media/suprateem_portfolio.45b48090.JPG"
+  },
+  {
+    "revision": "4a04f9f0dd3f59df96497cc7c3021550",
+    "url": "/react_portfolio/static/media/tie.4a04f9f0.svg"
+  },
+  {
+    "revision": "2185e423b1c8b70c671e1c927b95d4cd",
+    "url": "/react_portfolio/static/media/tweetme.2185e423.PNG"
+  },
+  {
+    "revision": "b46710ff8fbdb68ffa5cb5fdbf26c419",
+    "url": "/react_portfolio/static/media/ux.b46710ff.svg"
+  }
+]);
