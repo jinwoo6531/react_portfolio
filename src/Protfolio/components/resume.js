cb0f708daa6fee6780e50764076f99780cf4790c
jinwoo <jintakk6531@gmail.com>
2020-12-03 14:16:58 +0900

    12/3

--- src/Protfolio/components/resume.js
+++ src/Protfolio/components/resume.js
@@ -61,11 +61,6 @@ const Resume = () => {
               <h5>Frontend 개발자 양성과정</h5>
             </div>
             <div className="resume_card_year">비트교육센터 (2020)</div>
-            <p className="resume_card_description">
-              I am currently persuing B.tech in Computer Science Engineering
-              from Academy of Technology
-              <p>CGPA (till 5th sem) : 7.5 </p>
-            </p>
           </div>
         </div>
         {/* 경력 생기면 추가 */}
